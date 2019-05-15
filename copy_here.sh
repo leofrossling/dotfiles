4e2a881c89684f93a71131a2cb04eb04ec8ffe88
Leo Frössling <lefr1017@live.se>
2019-05-15 11:05:25 +0200

    Added more config files

---
+++ copy_here.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -r ~/.i3/* ./.i3/
+cp -r ~/.vimrc ./.vimrc
+cp -r ~/.bashrc ./bashrc
+cp -r ~/.Xresources ./.Xresources
+cp -r ~/.config/* ./.config/
---
+++ extract_from_here.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -r ./.i3/ ~/.i3/* 
+cp -r ./.Xresources ~/.Xresources
+cp -r ./.config/ ~/.config/*
