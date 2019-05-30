4c45235e2b810faf3445958512a03e8e124f4d32
Leo Frössling <leo@frossling.com>
2019-05-30 22:39:28 +0200

    Updated config files

--- extract_from_here.sh
+++ extract_from_here.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cp -r ./.i3/ ~/.i3/* 
+cp -r ./.i3/* ~/.i3/ 
 cp -r ./.Xresources ~/.Xresources
-cp -r ./.config/ ~/.config/*
+cp -r ./.config/compton.conf ~/.config/compton.conf
+cp -r ./.config/fish/* ~/.config/fish/
+cp -r ./.config/omf/* ~/.config/omf/
+cp -r ./.config/polybar/* ~/.config/polybar/
+cp -r ./.config/rofi/* ~/.config/rofi/
+cp -r ./.config/vis/* ~/.config/vis/
+cp -r ./.config/wal/* ~/.config/wal/
